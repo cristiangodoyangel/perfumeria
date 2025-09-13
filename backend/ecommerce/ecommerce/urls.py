72b0c07f15aacf24740efae20662abde6004034a
Cristian Godoy Angel <creacciones@gmail.com>
2025-09-13 15:50:58 -0300

    feat: Producos APIs funcionando

--- backend/ecommerce/ecommerce/urls.py
+++ backend/ecommerce/ecommerce/urls.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView, SpectacularAPIView
+from rest_framework.routers import DefaultRouter
+from usuarios.views import UsuarioViewSet
+from productos.views import ProductoViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+
+# DafaultRouter para manejar las rutas automáticamente
+router = DefaultRouter()
+router.register(r'usuarios', UsuarioViewSet, basename='usuario')  # Usuarios
+router.register(r'productos', ProductoViewSet, basename='producto')  # Productos
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('usuarios.urls')),  # Asegúrate de que la ruta esté correctamente incluida
+
+    # API endpoints
+    path('api/', include(router.urls)),
+
+    # Auth
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Otras rutas de tus aplicaciones...
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
